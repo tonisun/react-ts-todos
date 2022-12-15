aaaa10600b47d482acf211c357aeea2bedd1fa1a
Toni Zeidler <toni.z@gmx.de>
2022-12-15 15:49:37 +0100

    add new Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header text-bg-primary p-3'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover table-striped'>
+            <thead className='text-bg-info p-3' >
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem/>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,25 @@
+
+function TodoRowItem () {
+
+    return (
+        <>
+            <tr>
+                <th scope='row'>1</th>
+                <td>Feed cat</td>
+                <td>Toni</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td >Go shoping</td>
+                <td>Toni</td>
+              </tr>
+              <tr>
+                <th scope='row'>3</th>
+                <td>Feed cat</td>
+                <td>Toni</td>
+              </tr>
+        </>
+    )
+}
+
+export default TodoRowItem
