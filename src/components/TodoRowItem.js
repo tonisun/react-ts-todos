11e57a36c88c126cf081360ad02bc56cad1eec88
Toni Zeidler <toni.z@gmx.de>
2022-12-15 16:02:28 +0100

    static data

--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,22 +1,26 @@
 
 function TodoRowItem () {
 
+    const rowNumber = [1, 2, 3]
+    const rowDescription = ['Feed cat', 'Go shoping', 'Feed the Guppies']
+    const rowAssigned = ['Toni', 'Günter', 'Pepi'] 
+
     return (
         <>
             <tr>
-                <th scope='row'>1</th>
-                <td>Feed cat</td>
-                <td>Toni</td>
+                <th scope='row'>{rowNumber[0]}</th>
+                <td>{rowDescription[0]}</td>
+                <td>{rowAssigned[0]}</td>
               </tr>
               <tr>
-                <th scope='row'>2</th>
-                <td >Go shoping</td>
-                <td>Toni</td>
+                <th scope='row'>{rowNumber[1]}</th>
+                <td >{rowDescription[1]}</td>
+                <td>{rowAssigned[1]}</td>
               </tr>
               <tr>
-                <th scope='row'>3</th>
-                <td>Feed cat</td>
-                <td>Toni</td>
+                <th scope='row'>{rowNumber[2]}</th>
+                <td>{rowDescription[2]}</td>
+                <td>{rowAssigned[2]}</td>
               </tr>
         </>
     )
