7d624e5891b97b411f3ebd3833e91537fb04667f
Toni Zeidler <toni.z@gmx.de>
2022-12-15 16:25:08 +0100

    json
    json'.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import './App.css';
 import TodoRowItem from './components/TodoRowItem';
 
 function App() {
+
+  // JSON Objects
+  const arrTodos = [
+    {rowNumber: 1, rowDescription: 'Feed Aya', rowAssigned: 'Toni'},
+    {rowNumber: 2, rowDescription: 'Make sport', rowAssigned: 'Toni'},
+    {rowNumber: 3, rowDescription: 'Feed Guppies', rowAssigned: 'Pepi'},
+    {rowNumber: 4, rowDescription: 'Go shoping', rowAssigned: 'Günter'},
+    {rowNumber: 5, rowDescription: 'Lern Java Spring', rowAssigned: 'Toni'},
+    {rowNumber: 6, rowDescription: 'Wichsen', rowAssigned: 'Toni'},
+  ]
+
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -18,7 +29,36 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <TodoRowItem/>
+              <TodoRowItem 
+                rowNumber={arrTodos[0].rowNumber} 
+                rowDescription={arrTodos[0].rowDescription} 
+                rowAssigned={arrTodos[0].rowAssigned}
+              />
+              <TodoRowItem 
+                rowNumber={arrTodos[1].rowNumber} 
+                rowDescription={arrTodos[1].rowDescription} 
+                rowAssigned={arrTodos[1].rowAssigned}
+              />
+              <TodoRowItem 
+                rowNumber={arrTodos[2].rowNumber} 
+                rowDescription={arrTodos[2].rowDescription} 
+                rowAssigned={arrTodos[2].rowAssigned}
+              />
+              <TodoRowItem 
+                rowNumber={arrTodos[3].rowNumber} 
+                rowDescription={arrTodos[3].rowDescription} 
+                rowAssigned={arrTodos[3].rowAssigned}
+              />
+              <TodoRowItem 
+                rowNumber={arrTodos[4].rowNumber} 
+                rowDescription={arrTodos[4].rowDescription} 
+                rowAssigned={arrTodos[4].rowAssigned}
+              />
+              <TodoRowItem 
+                rowNumber={arrTodos[5].rowNumber} 
+                rowDescription={arrTodos[5].rowDescription} 
+                rowAssigned={arrTodos[5].rowAssigned}
+              />
             </tbody>
           </table>
         </div>
--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,27 +1,18 @@
 
-function TodoRowItem () {
+function TodoRowItem (props) {
 
-    const rowNumber = [1, 2, 3]
-    const rowDescription = ['Feed cat', 'Go shoping', 'Feed the Guppies']
-    const rowAssigned = ['Toni', 'Günter', 'Pepi'] 
+    //const rowNumber = [1, 2, 3]
+    //const rowDescription = ['Feed cat', 'Go shoping', 'Feed the Guppies']
+    //const rowAssigned = ['Toni', 'Günter', 'Pepi'] 
 
     return (
         <>
             <tr>
-                <th scope='row'>{rowNumber[0]}</th>
-                <td>{rowDescription[0]}</td>
-                <td>{rowAssigned[0]}</td>
-              </tr>
-              <tr>
-                <th scope='row'>{rowNumber[1]}</th>
-                <td >{rowDescription[1]}</td>
-                <td>{rowAssigned[1]}</td>
-              </tr>
-              <tr>
-                <th scope='row'>{rowNumber[2]}</th>
-                <td>{rowDescription[2]}</td>
-                <td>{rowAssigned[2]}</td>
-              </tr>
+                <th scope='row'>{props.rowNumber}</th>
+                <td>{props.rowDescription}</td>
+                <td>{props.rowAssigned}</td>
+            </tr>
+              
         </>
     )
 }
